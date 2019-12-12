f488d9eee7929a264e19012f062afb7a7cc9949f
Narendra Yalavarthi <nyalavarthi@gmail.com>
2019-12-12 15:26:49 -0500

    initial version

---
+++ main.tf
@@ -0,0 +1,10 @@
+module "s3_bucket" {
+  source                   = "git::https://github.com/cloudposse/terraform-aws-s3-bucket.git?ref=master"
+  enabled                  = true
+  user_enabled             = true
+  versioning_enabled       = false
+  allowed_bucket_actions   = ["s3:GetObject", "s3:ListBucket", "s3:GetBucketLocation"]
+  name                     = "terraform-module-sbox-bucket2"
+  stage                    = "test"
+  namespace                = "eg"
+}
