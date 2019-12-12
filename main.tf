37e0a84fab433a7040ab2a59501d1b5670420828
Narendra Yalavarthi <nyalavarthi@gmail.com>
2019-12-12 15:59:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
-module "s3_bucket" {
-  source                   = "https://github.com/clouddrove/terraform-aws-s3?ref=tags/0.12.2"
-  enabled                  = true
-  user_enabled             = true
-  versioning_enabled       = false
-  allowed_bucket_actions   = ["s3:GetObject", "s3:ListBucket", "s3:GetBucketLocation"]
-  name                     = "terraform-module-sbox-bucket2"
-  stage                    = "test"
-  namespace                = "eg"
+module "s3" {
+  source  = "clouddrove/s3/aws"
+  version = "0.12.2"
 }
